762228f5e022cd43e868db239a7a5ebf2771e502
Tasman <122511599+tasmanday@users.noreply.github.com>
2023-05-17 23:35:09 +0930

    added ex04

---
+++ C00_home/ex04/ft_is_negative.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void ft_is_negative(int n);
+
+int main(void)
+{
+	ft_is_negative(5);
+}
+
+void ft_is_negative(int n)
+{
+	if(n < 0)
+	{
+		char c = 'N';
+		write(1, &c, 1);
+	}
+	else
+	{
+		char c = 'P';
+		write(1, &c, 1);
+	}
+}
