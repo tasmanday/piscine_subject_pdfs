61c3a9abd637c4f7c848ee4872d96cea5af2ad2d
tasmanday <122511599+tasmanday@users.noreply.github.com>
2023-05-22 21:26:49 +0930

    added ex06

---
+++ c01/ex06/ft_strlen.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmitchel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 20:53:34 by tmitchel          #+#    #+#             */
+/*   Updated: 2023/05/22 21:24:27 by tmitchel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* #include <stdio.h> */
+
+int	ft_strlen(char *str);
+
+/* int	main(void)
+{
+	char	*str;
+	int		len;
+
+	str = "hello";
+	len = ft_strlen(str);
+	printf("strlen = %i \n", len);
+} */
+
+int	ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
