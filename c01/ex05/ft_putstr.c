651f1a377ca11d2da1ea8e701ca6d1d3744c4eda
tasmanday <122511599+tasmanday@users.noreply.github.com>
2023-05-22 20:51:04 +0930

    fixed ex05 to be only 1 function

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -6,23 +6,22 @@
 /*   By: tmitchel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 18:51:32 by tmitchel          #+#    #+#             */
-/*   Updated: 2023/05/22 19:38:11 by tmitchel         ###   ########.fr       */
+/*   Updated: 2023/05/22 20:49:58 by tmitchel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 void	ft_putstr(char *str);
-void	ft_putchar(char c);
 
-int	main(void)
+/* int	main(void)
 {
 	char	*str;
 
 	str = "hello";
 	ft_putstr(str);
 	return (0);
-}
+} */
 
 void	ft_putstr(char *str)
 {
@@ -31,12 +30,7 @@ void	ft_putstr(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		ft_putchar(str[i]);
+		write(1, &str[i], 1);
 		i++;
 	}
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
 }
