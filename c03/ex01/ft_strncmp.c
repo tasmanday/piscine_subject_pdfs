a36dc65207d91a0f63faa512be2ef9197002f611
Tasman <122511599+tasmanday@users.noreply.github.com>
2023-05-24 23:58:13 +0930

    modified ex01

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -30,5 +30,9 @@ int ft_strncmp(char *s1, char *s2, unsigned int n)
         }
         i++;
     }
-    return (s1[i] - s2[i]);
+    if (i < n)
+    {
+        return (s1[i] - s2[i]);
+    }
+    return (0);
 }
