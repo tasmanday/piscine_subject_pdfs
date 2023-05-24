5f976b1fff3ed2f661be0d36c053b1294fa69884
Tasman <122511599+tasmanday@users.noreply.github.com>
2023-05-24 23:49:16 +0930

    added ex01

---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,34 @@
+/* #include <stdio.h> */
+
+int ft_strncmp(char *s1, char *s2, unsigned int n);
+
+/* int main(void)
+{
+    char    *s1;
+    char    *s2;
+    int     cmp;
+    unsigned int    n;
+
+    s1 = "abcd";
+    s2 = "abc";
+    n = 2;
+    cmp = ft_strncmp(s1, s2, n);
+    printf("%i\n", cmp);
+    return (0);
+} */
+
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+    unsigned int     i;
+
+    i = 0;
+    while (i < n && s1[i] != '\0' && s2[i] != '\0')
+    {
+        if (s1[i] != s2[i])
+        {
+            return (s1[i] - s2[i]);
+        }
+        i++;
+    }
+    return (s1[i] - s2[i]);
+}
